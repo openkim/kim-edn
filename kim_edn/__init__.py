adc636d804076de551b2ac53cc761215893827df
Yaser Afshar <ya.afshar@gmail.com>
2024-03-10 14:39:11 -0700

    Fixes module level import & blank line warning

--- kim_edn/__init__.py
+++ kim_edn/__init__.py
@@ -142,9 +142,7 @@ Using kim_edn.tool from the shell to validate and pretty-print::
         }
     }
 
-"""
 
-"""
 Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 Python Software Foundation;
 All Rights Reserved
@@ -417,7 +415,5 @@ def loads(s, *, cls=None, parse_float=None, parse_int=None,
     return cls(**kw).decode(s)
 
 
-
-from . import _version
+from . import _version  # noqa: E402
 __version__ = _version.get_versions()['version']
-
