24df14e18d25b9941d2983fd868eb815b6df131e
Yaser Afshar <ya.afshar@gmail.com>
2024-03-20 21:04:40 -0700

    update the pyproject.toml & setup

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 """Setuptools based setup module."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
+
 import versioneer
+
 
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    zip_safe=True,
-    packages=find_packages(exclude=("tests.*", "tests")),
-    include_package_data=True,
 )
